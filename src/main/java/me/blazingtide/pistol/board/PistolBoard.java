35ee8e13119fe24a2576fa061ffa5874d761f396
BlazingTide <blazingtide@gmail.com>
2020-04-28 16:28:37 -0700

    finished basic structure // handlers

--- src/main/java/me/blazingtide/pistol/board/PistolBoard.java
+++ src/main/java/me/blazingtide/pistol/board/PistolBoard.java
@@ -1,15 +1,27 @@
 package me.blazingtide.pistol.board;
 
+import com.google.common.collect.Maps;
 import lombok.Getter;
 import me.blazingtide.pistol.Pistol;
+import me.blazingtide.pistol.board.entry.BoardEntry;
 import me.blazingtide.pistol.util.ColorUtil;
+import org.apache.commons.lang.CharUtils;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.scoreboard.DisplaySlot;
 import org.bukkit.scoreboard.Objective;
+import org.bukkit.scoreboard.Score;
 import org.bukkit.scoreboard.Scoreboard;
+
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ThreadLocalRandom;
 
 @Getter
 public class PistolBoard {
+
+    private final Map<String, BoardEntry> entries = Maps.newHashMap();
 
     private final Pistol pistol;
     private final Player player;
@@ -17,17 +29,59 @@ public class PistolBoard {
 
     private Objective objective;
 
-    public PistolBoard(Pistol pistol, Player player, Scoreboard scoreboard) {
+    public PistolBoard(Pistol pistol, Player player) {
         this.pistol = pistol;
         this.player = player;
-        this.scoreboard = scoreboard;
+        this.scoreboard = player.getScoreboard() != Bukkit.getScoreboardManager().getMainScoreboard() ? player.getScoreboard() : Bukkit.getScoreboardManager().getNewScoreboard();
     }
 
     public void init() {
-        objective = scoreboard.registerNewObjective("test", "dummy");
+        objective = scoreboard.registerNewObjective(player.getName(), "dummy");
         objective.setDisplaySlot(DisplaySlot.SIDEBAR);
         objective.setDisplayName(ColorUtil.translate(pistol.getAdapter().getTitle(player)));
-        
+
+        updateEntries();
+    }
+
+    private void updateEntries() {
+        buildEntries();
+
+        player.setScoreboard(scoreboard);
+    }
+
+    private void buildEntries() {
+        //Reset the scores every time you build entries
+        entries.keySet().forEach(scoreboard::resetScores);
+        entries.clear();
+
+        final List<String> lines = pistol.getAdapter().getLines(player);
+
+        //MC limits to 15 lines per scoreboard so we only want to do the first 15 lines
+        for (int i = 0; i < Math.min(lines.size(), 16); i++) {
+            final String line = lines.get(i);
+            final BoardEntry entry = BoardEntry.of(scoreboard, findId(""), ColorUtil.translate(line));
+
+            final Score score = objective.getScore(entry.getId());
+            score.setScore(i);
+
+            entries.put(entry.getId(), entry);
+        }
+    }
+
+    /**
+     * Creates a unique ID from chatcolor every time it's needed.
+     *
+     * @param start the start of the code.
+     * @return the unique Id
+     */
+    private String findId(String start) {
+        start += start + ChatColor.values()[ThreadLocalRandom.current().nextInt(0, ChatColor.values().length)];
+
+        if (entries.containsKey(start) || scoreboard.getTeam(start) != null) {
+            return findId(start);
+        }
+
+        return start;
     }
 
 }
--- src/main/java/me/blazingtide/pistol/board/entry/BoardEntry.java
+++ src/main/java/me/blazingtide/pistol/board/entry/BoardEntry.java
@@ -1,6 +1,9 @@
 package me.blazingtide.pistol.board.entry;
 
 import lombok.Data;
+import org.bukkit.ChatColor;
+import org.bukkit.scoreboard.Scoreboard;
+import org.bukkit.scoreboard.Team;
 
 @Data
 public class BoardEntry {
@@ -11,8 +14,17 @@ public class BoardEntry {
     private final String prefix;
     private final String suffix;
 
-    public static BoardEntry of(String id, String line) {
+    private final Team team;
 
+    public static BoardEntry of(Scoreboard scoreboard, String id, String line) {
+        final Team team = scoreboard.getTeam(id);
+        final String prefix = line.length() > 16 ? line.substring(0, 16) : line;
+        final String suffix = line.length() > 16 ? ChatColor.getLastColors(prefix) + line.substring(16, Math.min(16, line.length())) : "";
+
+        team.setPrefix(prefix);
+        team.setSuffix(suffix);
+
+        return new BoardEntry(id, line, prefix, suffix, team);
     }
 
 }
